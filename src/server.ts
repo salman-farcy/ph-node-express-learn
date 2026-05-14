a9ad5f882d23096c9b7725f9b6a1bbcb08ac9f97
slaman-farcy <salmanfarcy253@gmail.com>
2026-05-14 08:34:00 +0600

    using pxpress Middleware

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,10 @@ import express, { type Application, type Request, type Response } from "express"
 const app : Application = express()
 const port = 5000
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({extended : true}))
+
 
 app.get('/', (req : Request, res : Response) => {
   // res.send("Express Server")
