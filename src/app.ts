0609bb4ac4361ef90fad24626b3bff1e1bcfe679
slaman-farcy <salmanfarcy253@gmail.com>
2026-05-18 23:38:58 +0600

    create profiles in db

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 
 import express, { type Application, type Request, type Response } from "express"
-import { pool } from "./DB";
 import { userRoute } from "./modules/user/user.route";
 
 const app: Application = express()
@@ -8,6 +7,8 @@ const app: Application = express()
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }))
+
+
 
 app.get('/', (req: Request, res: Response) => {
   // res.send("Express Server")
